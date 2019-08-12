f7780a1ba5e9e1482fba1745472af3dbb915ea2b
Alfred <52985096+alfred404@users.noreply.github.com>
2019-08-12 15:35:16 -0400

    DONE: tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,15 @@
-const ftoc = function() {
+const ftoc = (temp) => {
+  const foo = (temp - 32) * (5 / 9);
+  return Math.round(foo * 10) / 10;
+};
 
-}
 
-const ctof = function() {
-
-}
+const ctof = (temp) => {
+  const foo = (temp * 1.8) + 32;
+  return Math.round(foo * 10) / 10;
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};
