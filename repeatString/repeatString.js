18681c9b5489e7cd089e1ffa3febb6b1f972586d
Alfred <52985096+alfred404@users.noreply.github.com>
2019-08-11 22:08:33 -0400

    DONE: repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (input, numberOfTimes) {
+    if (numberOfTimes < 0) {
+        return 'ERROR';
+    }
 
-}
+    let output = '';
+    for (let i = 0; i < numberOfTimes; i++) {
+        output += input;
+    }
 
-module.exports = repeatString
+    return output;
+};
+
+module.exports = repeatString;
