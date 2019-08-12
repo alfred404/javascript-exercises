e1e197ec9e8caeecd4d851d5854028ecdb305e5e
Alfred <52985096+alfred404@users.noreply.github.com>
2019-08-12 14:15:12 -0400

    DONE: sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = (num1, num2) => {
+  const invalidValues = [num1, num2].filter((x) => !Number.isInteger(x) || x < 0);
+  if (invalidValues.length > 0) { return 'ERROR'; }
 
-}
+  let sum = 0;
 
-module.exports = sumAll
+  const nums = [num1, num2].sort(); // ascending order -- smaller number comes first
+  const smaller = nums[0];
+  const larger = nums[1];
+
+  for (let i = smaller; i <= larger; i++) {
+    sum += i;
+  }
+
+  return sum;
+};
+
+module.exports = sumAll;
