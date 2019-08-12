2a616d1fc4e13f35aef8e1a82d0da78c03904033
Alfred <52985096+alfred404@users.noreply.github.com>
2019-08-11 22:15:48 -0400

    DONE: reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (input) {
+    let chars = [...input];
+    let output = [];
 
+    for (let i = chars.length - 1; i >= 0; i--) {
+        output.push(chars[i]);
+    }
+
+    return output.join('');
 }
 
 module.exports = reverseString
